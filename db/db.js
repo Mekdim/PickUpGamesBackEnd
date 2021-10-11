4d2ce5191b8a8a15c829f696d9621520b8632b8e
Emiwodew <mickeynig@gmail.com>
2021-10-11 21:25:26 +0200

    added functionality to db.js

--- db/db.js
+++ db/db.js
@@ -1,80 +1,97 @@
-const config = require('./config')
-const { Pool } = require('pg')
-
+const config = require("./config");
+const { Pool } = require("pg");
 
 class db {
-    constructor(options) {
-        this.pool = new Pool(options);
+  constructor(options) {
+    this.pool = new Pool(options);
+  }
+
+  async addSession(values, fields) {
+    const client = await this.pool.connect();
+    const sessionQuery = {
+      text: "INSERT INTO Sessions (pitch_id, start_time, end_time, duration, number_of_players,price) values($1, $2, $3, $4, $5, $6) RETURNING id",
+      values: [...values],
+      rowMode: "array",
+    };
+    const sessionMembersQuery = {
+      text: "INSERT INTO session_members (player_id, session_id) values($1, $2)",
+      values: [...fields],
+      rowMode: "array",
+    };
+
+    try {
+      client.query("BEGIN");
+      const result1 = await client.query(sessionQuery);
+      sessionMembersQuery.values.push(result1.rows[0].id);
+      const result2 = await client.query(sessionMembersQuery);
+      await client.query("COMMIT");
+
+      return [result1, result2.rows];
+    } catch (error) {
+      console.log("Error ", error);
+      try {
+        await client.query("ROLLBACK");
+      } catch (rollbackError) {
+        console.log("A rollback error occurred:", rollbackError);
+      }
+      throw error;
+    } finally {
+      client.release();
     }
+  }
 
-    async addSession(values, fields) {
-        const client = await this.pool.connect()
-        const sessionQuery = {
-            text: 'INSERT INTO Sessions (pitch_id, start_time, end_time, duration, number_of_players,price) values($1, $2, $3, $4, $5, $6) RETURNING id',
-            values: [...values],
-            rowMode: 'array',
-        };
-        const sessionMembersQuery = {
-            text: 'INSERT INTO session_members (player_id, session_id) values($1, $2)',
-            values: [...fields],
-            rowMode: 'array',
-        };
+  async joinSession(val1, val2) {
+    const client = await this.pool.connect();
+    const sessionUpdate = {
+      text: "UPDATE Sessions set number_of_players = number_of_players+1 where id=$1",
+      values: [...val1],
+      rowMode: "array",
+    };
+    const sessionMembersJoin = {
+      text: "INSERT INTO session_members (player_id, session_id) values($1, $2)",
+      values: [...val2],
+      rowMode: "array",
+    };
 
-        try {
-            client.query('BEGIN');
-            const result1 = await  client.query(sessionQuery);
-            sessionMembersQuery.values.push(result1.rows[0].id);
-            const result2 = await client.query(sessionMembersQuery);
-            await client.query('COMMIT')
+    try {
+      client.query("BEGIN");
+      const result1 = await client.query(sessionUpdate);
+      const result2 = await client.query(sessionMembersJoin);
+      await client.query("COMMIT");
 
-            return [result1, result2.rows];
-
-        }catch (error) {
-            console.log("Error ", error);
-            try {
-                await client.query('ROLLBACK');
-            }catch (rollbackError) {
-                console.log('A rollback error occurred:', rollbackError);
-            }
-            throw error;
-        } finally {
-            client.release()
-        }
+      return [result1, result2.rows];
+    } catch (error) {
+      console.log("Error ", error);
+      try {
+        await client.query("ROLLBACK");
+      } catch (rollbackError) {
+        console.log("A rollback error occurred:", rollbackError);
+      }
+      throw error;
+    } finally {
+      client.release();
     }
+  }
 
-    async joinSession(val1, val2) {
-        const client = await this.pool.connect()
-        const sessionUpdate = {
-            text: 'UPDATE Sessions set number_of_players = number_of_players+1 where id=$1',
-            values: [...val1],
-            rowMode: 'array',
-        };
-        const sessionMembersJoin = {
-            text: 'INSERT INTO session_members (player_id, session_id) values($1, $2)',
-            values: [...val2],
-            rowMode: 'array',
-        };
+  async findSession(sessionId, date = new Date()) {
+    //TODO add date to the query values too
+    const query = {
+      text: "SELECT * FROM Sessions WHERE pitch_id = $1",
+      values: [sessionId],
+    };
 
-        try {
-            client.query('BEGIN');
-            const result1 = await  client.query(sessionUpdate);
-            const result2 = await client.query(sessionMembersJoin);
-            await client.query('COMMIT')
+    const client = await this.pool.connect();
 
-            return [result1, result2.rows];
-
-        }catch (error) {
-            console.log("Error ", error);
-            try {
-                await client.query('ROLLBACK');
-            }catch (rollbackError) {
-                console.log('A rollback error occurred:', rollbackError);
-            }
-            throw error;
-        } finally {
-            client.release()
-        }
+    try {
+      const results = await client.query(query);
+      return results.rows;
+    } catch (error) {
+      console.log("unable to query Sessions ", error);
+      throw error;
+    } finally {
+      client.release();
     }
+  }
 }
 
 const database = new db(config.database);
--- routes/pitch.js
+++ routes/pitch.js
@@ -1,57 +1,58 @@
-var express = require('express');
-var router = express.Router();
-const config = require('../db/config')
-const { Pool } = require('pg')
+const express = require("express");
+const router = express.Router();
 const database = require("../db/db");
-const pool = new Pool(config.database)
 
+router.post("/addSession", async function (req, res, next) {
+  try {
+    // TODO perform some data validation
+    let val1 = [
+      req.body.pitch_id,
+      req.body.start_time,
+      req.body.end_time,
+      req.body.duration,
+      req.body.number_of_players,
+      req.body.price,
+    ];
+    let val2 = [req.body.player_id];
+    let results = await database.addSession(val1, val2);
 
-router.post('/addSession', async function(req, res, next) {
-    try{
-        // TODO perform some data validation
-        let val1 =  [req.body.pitch_id,req.body.start_time, req.body.end_time, req.body.duration,req.body.number_of_players,req.body.price];
-        let val2 =  [req.body.player_id];
-        let results = await database.addSession(val1, val2);
+    console.info(results);
 
-        console.info(results);
-
-        res.status(201).json({
-            status:"success"
-        })
-    }catch(err){
-        console.log(err)
-        res.status(500).json(err)
-    }
-});
-
-router.put('/joinSession', async function(req, res, next) {
-  console.log(req.body)
-  try{
-
-      let val1 = [req.body.session_id];
-      let val2 = [req.body.player_id,req.body.session_id];
-      const results = await database.joinSession(val1, val2);
-
-      console.info(results);
-      res.status(201).json({
-        status:"success"
-      })
-  }catch(err){
-     console.log(err)
-     res.status(500).json(err)
+    res.status(201).json({
+      status: "success",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
-router.get('/sessions/:pitch_id', async function(req, res, next) {
-  console.log(req.params.pitch_id)
-  console.log("mek for pitch id")
+router.put("/joinSession", async function (req, res, next) {
+  console.log(req.body);
+  try {
+    let val1 = [req.body.session_id];
+    let val2 = [req.body.player_id, req.body.session_id];
+    const results = await database.joinSession(val1, val2);
 
-  try{
-      const results = await pool.query('SELECT * FROM Sessions WHERE pitch_id = $1', [req.params.pitch_id] )
-      res.status(201).json(results.rows)
-  }catch(err){
-     console.log(err)
-     res.status(500).json(err)
+    console.info(results);
+    res.status(201).json({
+      status: "success",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/sessions/:pitch_id", async function (req, res, next) {
+  console.log(req.params.pitch_id);
+
+  try {
+    const results = await database.findSession(req.params.pitch_id);
+    res.status(201).json(results);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
